cc03ccc7dc469a4a00e449df7080e68be4bc210c
Andrey <andrejsatykov@yandex.ru>
2022-12-01 00:41:38 +0300

    The third digit Task 02

---
+++ Task_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, 
+// которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < 99)
+{
+    Console.WriteLine("Заданное число не является трехзначным, третьей цифры нет.");
+}
+else
+{
+    char [] array = num.ToString().ToCharArray();
+    Console.WriteLine($"Третья цифра заданного числа {array[2]}. ");
+}
+
+ 
+
